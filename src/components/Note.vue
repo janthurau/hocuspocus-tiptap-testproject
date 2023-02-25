84c43c8e7331377d95ee7d274587a98f14a6841a
Jan Thurau <jan@janthurau.de>
2023-02-25 15:44:20 +0100

    initial commit, tiptap working

---
+++ src/components/Note.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useEditor, EditorContent } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit'
+
+const editor = useEditor({
+  content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
+  extensions: [
+    StarterKit,
+  ],
+  editorProps: {
+    attributes: {
+      class: 'border-2 border-yellow-500 bg-yellow-300',
+    },
+  },
+})
+
+</script>
+
+<template>
+  <editor-content :editor="editor" />
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Note from "@/components/Note.vue";
+</script>
+
+<template>
+  <main>
+    <Note class="my-5"/>
+  </main>
+</template>
