58878e6d387994590a07f7d1b32590fc95023af2
Jan Thurau <jan@janthurau.de>
2024-02-20 18:09:54 +0100

    changes for comments/threads

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
-import {useEditor, EditorContent} from '@tiptap/vue-3'
+import {useEditor, EditorContent, findParentNode, Extension} from '@tiptap/vue-3';
 import StarterKit from '@tiptap/starter-kit'
-import {Collaboration} from "@tiptap/extension-collaboration";
-import Image from '@tiptap/extension-image'
-import {HocuspocusProvider, HocuspocusProviderWebsocket} from '@hocuspocus/provider'
-import {onUnmounted, ref} from "vue";
-import {CollaborationCursor} from "@tiptap/extension-collaboration-cursor";
+import {
+  HocuspocusProvider,
+  HocuspocusProviderWebsocket,
+  TiptapCollabProvider,
+  type TCollabThread
+} from '@hocuspocus/provider'
+import {onMounted, onUnmounted, ref} from "vue";
 import {TiptapTransformer} from "@hocuspocus/transformer";
+import * as Y from 'yjs'
+import Collaboration from "@tiptap/extension-collaboration";
+import CollaborationCursor from "@tiptap/extension-collaboration-cursor";
+import CodeBlock from '@tiptap/extension-code-block'
 
 // add new prop title which is a number
 const props = defineProps<{ name: string, websocket: HocuspocusProviderWebsocket }>()
@@ -16,12 +22,14 @@ const status = ref('')
 const synced = ref('')
 const currentVersion = ref(0)
 
-// create a method that returns a random number between variables min and max that are passed to the method
+window.Y = Y
 
-const provider = new HocuspocusProvider({
-  name: `test-${props.name}`,
+const provider = new TiptapCollabProvider({
+  name: `threads-${props.name}`,
+  broadcast: false,
   websocketProvider: props.websocket,
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.EMfozF498NIwz9weJPF3d_AVSRYS22HE39T8E4qfLI8',
+// token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.iNVzTzLphhDcpjuIMnHz2ZZpmmlHxFES-FtEp-JnOrE',
+  // token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDE3MTI4MzIsIm5iZiI6MTcwMTcxMjgzMiwiZXhwIjoxNzAxNzk5MjMyLCJpc3MiOiJodHRwczovL2NvbGxhYi50aXB0YXAuZGV2IiwiYXVkIjoieHk5ZGo5ZTYifQ.GLKATyfk6zMySW7D1ACdqc4eS6iBq0yrlugs4A1xmKg',
   onStateless(data) {
     const payload = JSON.parse(data.payload)
 
@@ -32,17 +40,46 @@ const provider = new HocuspocusProvider({
       case 'version.created':
         currentVersion.value = payload.version
         break;
+      case 'version.preview':
+        const tempYdoc = new Y.Doc()
+        Y.applyUpdate(tempYdoc, Uint8Array.from(atob(payload.ydoc), c => c.charCodeAt(0)))
+
+        console.log(payload.version)
+        console.log(TiptapTransformer.fromYdoc(tempYdoc, 'default'))
+
+        break;
     }
   },
+  onClose(data) {
+    console.log(`CLOSE`, data.event.code, data.event.reason)
+
+    if (data.event.reason === 'Document deleted') {
+      console.log('disconnecting')
+      // provider.destroy()
+    }
+  },
+
+  onMessage(data) {
+    // console.log(data)
+  },
   onStatus(data) {
-    console.log('status', data)
+    // console.log('status', data)
     status.value = data.status
   },
   onSynced(data) {
+    console.log('synced')
     // called when the document is synced
     // terminate the editor once this is called
     synced.value = data.state ? "yes" : "no"
   },
+
+  onClose(data) {
+    console.log(data)
+  }
+})
+
+provider.on('unsyncedChanges', (e) => {
+  // console.log(e)
 })
 
 type TAuditHistoryVersion = {
@@ -51,14 +88,13 @@ type TAuditHistoryVersion = {
   date: number;
 }
 
-
 const versions = ref<TAuditHistoryVersion[]>([])
 const versioningStatus = ref(false)
 
-provider.document.getArray<TAuditHistoryVersion>('__tiptapcollab__versions').observe(e => {
+provider.watchVersions(e => {
   versions.value = provider.document.getArray<TAuditHistoryVersion>('__tiptapcollab__versions').toArray()
 })
-
+//
 provider.document.getMap<number>('__tiptapcollab__config').observe(e => {
   versioningStatus.value = !!provider.document.getMap<number>('__tiptapcollab__config').get('autoVersioning')
 })
@@ -68,51 +104,164 @@ window.providers[props.name] = provider
 
 onUnmounted(() => provider.destroy())
 
+onMounted(() => {
+
+  Collaboration.configure({
+    document: provider.document,
+  })
+
+
+  CollaborationCursor.configure({
+    provider: provider,
+    user: {
+      name: "Andrew Arkhipdov",
+      color: "#ff66b3",
+    },
+  })
+})
+const previousNode = ref()
+const aiLoading = ref(false)
+
+const AiBotExtension = Extension.create({
+  name: 'aiBotExtension',
+  priority: 1000,
+
+  onSelectionUpdate(args) {
+    return
+
+    const currentNodePosition = args.editor.state.selection.$anchor.pos
+
+    if (currentNodePosition < 0) {
+      return
+    }
+
+    const currentNode = args.editor.state.selection.$anchor.parent
+    try {
+      const currentNodeId = currentNode.attrs.uid
+
+      const previousNodeId = previousNode.value?.attrs.uid
+
+      if (currentNodeId
+        && previousNodeId
+        && previousNodeId !== currentNodeId
+        && previousNode.value.textContent
+      ) {
+        console.log(`sending node ID for AI action: ${previousNodeId}`)
+
+        aiLoading.value = true
+        provider.sendStateless(JSON.stringify({
+          action: 'ai',
+          nodeId: previousNodeId,
+        }))
+      }
+
+    } catch (e) {
+      console.error(e)
+    }
+
+    previousNode.value = currentNode
+  }
+
+})
+
 const editor = useEditor({
   extensions: [
     StarterKit.configure({
-      history: false
+      history: false,
+      codeBlock: false
     }),
     Collaboration.configure({
       document: provider.document,
     }),
-    Image.configure({
-      inline: true,
-    })    // CollaborationCursor.configure({
-    //   provider: provider,
-    //   user: {
-    //     name: 'Cyndi Lauper',
-    //     color: '#f783ac',
-    //   },
+    CodeBlock.configure({
+      HTMLAttributes: {
+        class: 'bg-gray b-2 b-red',
+      },
+    }),
+    CollaborationCursor.configure({
+      provider: provider,
+      user: {
+        name: "Andrew Arkhipov",
+        color: "#ff66b3",
+      },
+    }),
+
+    // UniqueId.configure({
+    //   types: ['paragraph'],
+    //   attributeName: 'uid',
     // }),
+    // AiBotExtension.configure(),
   ],
+  onCreate(args) {
+    // applyDevTools(args.editor.view);
+  },
   editorProps: {
     attributes: {
-      class: 'text-4xl bg-yellow-300 w-64 h-64 overflow-hidden p-2 shadow-lg'
+      class: 'border-2 border-black p-4 rounded-md h-96'
     },
   },
+  autofocus: 'start',
 })
+window.editors ??= {}
+window.editors[props.name] = editor
 
 const previewVersion = (version: number) => {
   console.log(`fetching preview for ${version}`)
+
+  provider.sendStateless(JSON.stringify({action: 'version.preview', version}))
 }
 
 const revertTo = (version: number) => {
   console.log(`reverting to ${version}`)
 
-  provider.sendStateless(JSON.stringify({action: 'revertTo', version}))
+  // provider.revertToVersion(version)
+  return provider.sendStateless(JSON.stringify({action: 'document.revert', version}))
+
 }
 
 const toggleVersioning = () => {
   console.log('updating')
-  provider.document.getMap<number>('__tiptapcollab__config').set('autoVersioning', versioningStatus.value ? 0 : 1)
+
+  switch (provider.isAutoVersioning()) {
+    case true:
+      provider.disableAutoVersioning()
+      break
+    case false:
+      provider.enableAutoVersioning()
+      break
+  }
+
 }
 
 const triggerVersioning = () => {
   console.log('triggering version')
-  provider.sendStateless(JSON.stringify({action: 'version.create', name: 'my custom name'}))
+  provider.createVersion()
 }
 
+provider.on('stateless', (payload) => {
+
+  try {
+    const jsonPayload = JSON.parse(payload.payload)
+
+    if (jsonPayload.event === 'ai.done') {
+      aiLoading.value = false
+    }
+  } catch (e) {
+    console.error(e)
+  }
+
+
+})
+
+const threads = ref<TCollabThread[]>()
+
+provider.watchThreads((a, b) => {
+  threads.value = provider.getThreads()
+})
+
+onMounted(() => {
+  threads.value = provider.getThreads()
+})
 
 </script>
 
@@ -126,7 +275,56 @@ const triggerVersioning = () => {
         {{ versioningStatus }})
       </button>
       <button @click.prevent="triggerVersioning" class="border bg-gray-200 p-1.5">Manually create version</button>
+      <button @click.prevent="provider.sendStateless(JSON.stringify({
+          action: 'ai',
+        }))" class="border bg-gray-200 p-1.5">SSE
+      </button>
+
+
+      <button @click.prevent="provider.createThread({data: {rrr: 'fff'}})" class="border bg-gray-200 p-1.5">Create
+        Thread
+      </button>
+      <button @click.prevent="provider.updateThread(provider.getThreads()[0].id, {data: {k: new Date().toISOString()}})"
+              class="border bg-gray-200 p-1.5">Update Thread
+      </button>
+      <button @click.prevent="provider.deleteThread(provider.getThreads()[0].id)" class="border bg-gray-200 p-1.5">
+        Delete Threads
+      </button>
+
+      <button @click.prevent="provider.addComment(provider.getThreads()[0].id, { data: { commentText: 'JA MOIN'}})" class="border bg-gray-200 p-1.5">Add
+        comment
+      </button>
+      <button @click.prevent="provider.updateComment(provider.getThreads()[0].id, provider.getThreads()[0].comments[0].id, {data: {update: (new Date()).toISOString()}})" class="border bg-gray-200 p-1.5">
+        Update comment
+      </button>
+      <button @click.prevent="provider.deleteComment(provider.getThreads()[0].id, provider.getThreads()[0].comments[0].id)" class="border bg-gray-200 p-1.5">
+        Delete comment
+      </button>
     </div>
+
+
+    <div>
+
+      <div
+        v-for="thread in threads" :key="thread.id">
+        Thread: {{ thread.id }} ///
+        {{ thread.updatedAt }} --- {{ JSON.stringify(thread.data) }}
+
+        <div class="ml-5">
+
+          <div
+            v-for="comment in thread.comments" :key="comment.id">
+            Comment: {{ comment.id }} // {{ comment.updatedAt }}
+          </div>
+
+        </div>
+
+
+      </div>
+
+
+    </div>
+
 
     <p class="mt-2">Last saved: <span v-if="versions.length > 0">{{
         (new Date(versions[versions.length - 1].date)).toISOString()
@@ -147,15 +345,15 @@ const triggerVersioning = () => {
           {{ version.version }}
         </span>
 
-        <span>
+          <span>
           [{{ (new Date(version.date)).toISOString() }}]
         </span>
 
-        <span>
+          <span>
           {{ version.name }}
         </span>
 
-        <span>
+          <span>
           <a @click.prevent="previewVersion(version.version)" href="" class="cursor-pointer font-bold">preview</a>
           or
           <a @click.prevent="revertTo(version.version)" href="" class="cursor-pointer font-bold">revertTo</a>
@@ -188,7 +386,21 @@ const triggerVersioning = () => {
       @click="provider.sendStateless(JSON.stringify({action: 'printLastUpdate'}))">printLastUpdate
     </button>
 
-    <editor-content :editor="editor" style="font-family: 'Reenie Beanie'"/>
+    {{ props.name }}<br/>
+
+    AI Loading: {{ aiLoading }}
+
+    <editor-content :editor="editor"/>
     <!--      <textarea class="text-4xl bg-yellow-300 w-64 h-64 overflow-hidden p-2 shadow-lg"></textarea>-->
   </div>
 </template>
+
+<style>
+.tiptap pre {
+  background: #0D0D0D;
+  color: #fff;
+  font-family: JetBrainsMono, monospace;
+  padding: 0.75rem 1rem;
+  border-radius: 0.5rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import Note from "@/components/Note.vue";
-import {HocuspocusProviderWebsocket} from '@hocuspocus/provider'
+import {HocuspocusProviderWebsocket, TiptapCollabProviderWebsocket} from '@hocuspocus/provider'
 import {onMounted, ref} from "vue";
 
 const status = ref('')
 const websocket = new HocuspocusProviderWebsocket({
+  // url: 'wss://xy9dj9e6.collab.tiptap.cloud',
   url: 'ws://127.0.0.1:8080',
+  // appId: 'XY9DJ9E6',
+  // appId: '8MZQQ3MX',
   onStatus(data) {
     status.value = data.status
+  },
+  onMessage(data){
+    // console.log(data)
+  },
+  onClose(data){
+    console.log(data)
   }
 })
 
@@ -15,11 +24,10 @@ type TNoteConfig = {
   name: string
 }
 
-const notes = ref<TNoteConfig[]>([
-])
+const notes = ref<TNoteConfig[]>([])
 
 const add = () => {
-  notes.value.push({name: `test${notes.value.length}`})
+  notes.value.push({name: `${notes.value.length}`})
   localStorage.setItem('noteCount', notes.value.length.toString())
 }
 
@@ -37,7 +45,7 @@ onMounted(() => {
   const notesToAdd = parseInt(localStorage.getItem('noteCount') ?? '1')
 
   for( let i = 0; i<notesToAdd ; i++){
-    notes.value.push({name: `test${i}`})
+    notes.value.push({name: `${i}`})
   }
 })
 
@@ -61,7 +69,7 @@ onMounted(() => {
       <p>{{status}}</p>
     </div>
 
-    <div class="grid grid-cols-1 gap-3">
+    <div class="grid grid-cols-2 gap-3 px-10">
       <Note
         v-for="(note, key) in notes"
         class="my-5" :name="note.name" :key="key" :websocket="websocket"/>
